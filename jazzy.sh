36c07151e0365b2ddaf88061473f55a6577cf7c5
zonble <zonble@gmail.com>
2018-02-04 01:14:34 +0800

    Updates documentation.

---
+++ jazzy.sh
@@ -0,0 +1,9 @@
+jazzy \
+  --author KKBOX \
+  --author_url https://developer.kkbox.com \
+  --github_url https://github.com/zonble/OpenAPI-Swift-Promises \
+  --github-file-prefix https://github.com/zonble/OpenAPI-Swift-Promises/tree/1.0.0 \
+  --module-version 1.0.0 \
+  --xcodebuild-arguments -scheme,KKBOXOpenAPISwiftPromises \
+  --module KKBOXOpenAPISwiftPromises
+  
